8d7836788b67de4e112a8181120ceaf37b68094c
Kevin <kevhines@gmail.com>
2021-01-25 17:18:35 -0500

    commit tree 6ed73a845457012867ec23be7ad37420fa9922ae

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,34 @@
 
+class Doctor
+
+attr_reader :name
+
+@@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.doctor == self
+        end
+  
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.map do |appt|
+            appt.patient
+        end.uniq
+    end
+
+    def self.all
+        @@all
+    end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,31 @@
 
+class Patient
+
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.patient == self
+        end
+    end
+
+    def doctors
+        appointments.map do |appt|
+            appt.doctor
+        end.uniq
+    end
+
+    def self.all
+        @@all
+    end
+end
